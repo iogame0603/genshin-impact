2abe89ce3a420165d7b2ecbf18a008ae1c0d6805
iogame <young312492@gmail.com>
2023-01-25 21:01:42 +0900

    first commit

--- genshin_notices(discord webhook).py
+++ genshin_notices(discord webhook).py
@@ -7,23 +7,24 @@ import requests, time
 discord_webhook_url = "https://discord.com/api/webhooks/1067436871724048394/uJSbkAjghNufdLU3GOASsUFDdY5btfLAdfR_jCeTzqBS1HBexbCIZDPRIu6kQFwVs4zG"
 genshin_cafe_api = "https://apis.naver.com/cafe-web/cafe2/NoticeListV3.json?cafeId=29893655&ad=true&mobileWeb=true&adUnit=MW_CAFE_BOARD&uuid=08eb7691-dd6c-421a-b71d-5aed7a867187"
 
-genshin_notices = []
 genshin_notices_copy = []
 genshin_notices_summary = []
 
-# while True:
-  # time.sleep(2)
+while True:
+  genshin_notices = []
+  r = requests.get(genshin_cafe_api)
+  j = r.json()
+  for i in range(5):
+    genshin_notices.append(j['message']['result']['articleList'][i]['item']['subject'])
+    genshin_notices_summary.append(j['message']['result']['articleList'][i]['item']['summary'])
 
-r = requests.get(genshin_cafe_api)
-j = r.json()
-for i in range(5):
-  genshin_notices.append(j['message']['result']['articleList'][i]['item']['subject'])
-  genshin_notices_summary.append(j['message']['result']['articleList'][i]['item']['summary'])
+  genshin_notices_copy = genshin_notices.copy()
 
-genshin_notices_copy = genshin_notices.copy()
+  data = {
+    'content': f"{genshin_notices[0]}\n\n{genshin_notices_summary[0]}"
+  }
 
-data = {
-  'content': genshin_notices[0]
-}
-
-# res = requests.post(discord_webhook_url, json=data)
+  if genshin_notices_copy[0] != genshin_notices[0]:
+    res = requests.post(discord_webhook_url, json=data)
+  
+  time.sleep(2)
